c013cf445c4f7e10271deb8414f407c6b4134836
kingofvibe <vibecity112@gmail.com>
2023-05-21 21:40:21 +0100

    desktop 90% baby

---
+++ index.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+const timeline = gsap.timeline({duration: 1})
+
+
+gsap.to('.middle', {
+    duration: 5,
+    opacity: 0,
+    ease: 'power4.out'
+})
+
+timeline.to('.l-box', {
+    duration: 1.5,
+    y: '-100%',
+    stagger: .5,
+    ease: 'power4.out'
+})
+timeline.to('.loader,.l-box-c', {
+    duration: .2,
+    zIndex: "-1"
+})
+timeline.from('.nav', {
+        duration: 1, 
+        y: '-100%',
+        ease: 'bounce'
+    })
+
+timeline.from('li, a', {
+        duration: 1,
+        y: '100%',
+        opacity: 0,
+        // delay: 1
+    })
+
+timeline.from('.hero-f-txt', {
+    duration: 1,
+    y: '-100%',
+    opacity: 0,
+    ease: 'bounce',
+    // delay: 2
+})
+
+timeline.from('.arrow-down', {
+    duration: 1,
+    y: '-100%',
+    opacity: 0,
+    ease: 'bounce',
+    // delay: 3
+})
